b2264daebe4f9dcedd9f74879114a39fdbd03e94
Mattia <matteron96@gmail.com>
2020-01-18 17:28:40 -0500

    Separated exports into variables to allow imports

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
-module.exports = (useSvg) => {
+const haipa = (useSvg) => {
 	const data = require('./data/data')(useSvg);
 	return {
 		tags: require('./factories/tags')(data.tags),
 		attr: require('./factories/attr')(data.attr)
 	}
 }
+
+module.exports = haipa;
