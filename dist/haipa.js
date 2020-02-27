791ffbcc9d818557185c400c3746856653c11b68
matteron <matteron96@gmail.com>
2020-02-27 11:54:11 -0500

    Browserify Build for 1.0.4

--- dist/haipa.js
+++ dist/haipa.js
@@ -51,101 +51,197 @@ exports.ariaAttr = [
 
 
 },{}],2:[function(require,module,exports){
+exports.baseTags = [
+	//'html', Handeled manually to prepend <!DOCTYPE html>
+	// Document Metadata
+	'base', 'link', 'meta', 'head', 'style', 'title', 
+	// Sectioning Root
+	'body',
+	// Content Sectioning
+	'address', 'article', 'aside', 'footer', 'header', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'hgroup', 'main', 'nav', 'section',
+	// Text Content
+	'blockquote', 'dd', 'div', 'dl', 'dt', 'figcaption', 'figure', 'hr', 'li', 'ol', 'p', 'pre', 'ul',
+	// Inline Text Semantics
+	'a', 'abbr', 'b', 'bdi', 'bdo', 'br', 'cite', 'code', 'data', 'dfn', 'em', 'i', 'kbd', 'mark', 'q','rb',
+	'rp', 'rt', 'rtc', 'ruby', 's', 'samp', 'small', 'span', 'strong', 'sub', 'sup', 'time', 'u', 'var', 'wbr',
+	// Image and Multimedia
+	'area', 'audio', 'img', 'map', 'track', 'video',
+	// Embedded Content
+	'embed', 'iframe', 'object', 'param', 'picture', 'source',
+	// Scripting
+	'canvas', 'noscript', 'script',
+	// Demarcating Edits
+	'del', 'ins',
+	// Table Content
+	'caption', 'col', 'colgroup', 'table', 'tbody', 'td', 'tfoot', 'th', 'thead', 'tr',
+	// Forms
+	'button', 'datalist', 'fieldset', 'form', 'input', 'label', 'legend', 'meter', 'optgroup', 'option', 'output', 'progress', 'select', 'textarea',
+	// Interactive Elements
+	'details', 'dialog', 'menu', 'summary',
+	// Web Components
+	'slot', 'template'
+];
 
+exports.baseAttr = [
+	// A
+	'abbr', 'accept', 'acceptCharset', 'accessKey', 'action', 'allowFullScreen', 'allowTransparency', 'alt', 'as', 'async', 'autoComplete', 'autoFocus', 'autoPlay',
+	// C  NOTE: 'class' is replaced with 'classes' due to name collision.
+	'cellPadding', 'cellSpacing', 'challenge', 'charset', 'checked', 'cite', 'className', 'cols', 'colSpan', 'command', 'content', 'contentEditable',
+	'contextMenu', 'controls', 'coords', 'crossOrigin',
+	// D
+	'data', 'dateTime', 'default', 'defer', 'dir', 'disabled', 'download', 'draggable', 'dropzone',
+	// E
+	'encType',
+	// F  NOTE: 'for' is replaced with 'isFor' due to name collision.
+	'form', 'formAction', 'formEncType', 'formMethod', 'formNoValidate', 'formTarget', 'frameBorder',
+	// H
+	'headers', 'height', 'hidden', 'high', 'href', 'hrefLang', 'htmlFor', 'httpEquiv',
+	// I
+	'icon', 'id', 'inputMode', 'isMap', 'itemId', 'itemProp', 'itemRef', 'itemScope', 'itemType',
+	// K
+	'kind',
+	// L
+	'label', 'lang', 'list', 'loop',
+	// M
+	'manifest', 'max', 'maxLength', 'media', 'mediaGroup', 'method', 'min', 'minLength', 'multiple', 'muted',
+	// N
+	'name', 'noValidate',
+	// O
+	'open', 'optimum',
+	// P
+	'pattern', 'ping', 'placeholder', 'poster', 'preload',
+	// R
+	'radioGroup', 'readOnly', 'rel', 'required', 'role', 'rows', 'rowSpan',
+	// S
+	'sandbox', 'scope', 'scoped', 'scrolling', 'seamless', 'selected', 'shape', 'size', 'sizes', 'sortable', 'span', 'spellCheck', 'src', 'srcDoc', 'srcSet',
+	'start', 'step', 'style',
+	// T
+	'tabIndex', 'target', 'title', 'translate', 'type', 'typeMustMatch',
+	// U
+	'useMap',
+	// V
+	'value', 'version',
+	// W
+	'width', 'wmode', 'wrap'
+];
+},{}],3:[function(require,module,exports){
 // Based on MDN List
 // https://developer.mozilla.org/en-US/docs/Web/HTML/Element
+const { baseTags, baseAttr } = require('./base');
+const { svgTags, svgAttr } = require('./svg');	
+const { ariaAttr } = require('./aria');
+const { eventAttr } = require('./events');
 
-module.exports = (useSvg) => {
-	const { svgTags, svgAttr } = useSvg ? require('./svg') : { svgTags: [], svgAttr: [] };	
-	const { ariaAttr } = require('./aria');
+exports.tagList = [
+	...baseTags,
+	// SVG
+	...svgTags
+];
 
-	const tagList = [
-		//'html', Handeled manually to prepend <!DOCTYPE html>
-		// Document Metadata
-		'base', 'link', 'meta', 'head', 'style', 'title', 
-		// Sectioning Root
-		'body',
-		// Content Sectioning
-		'address', 'article', 'aside', 'footer', 'header', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'hgroup', 'main', 'nav', 'section',
-		// Text Content
-		'blockquote', 'dd', 'div', 'dl', 'dt', 'figcaption', 'figure', 'hr', 'li', 'ol', 'p', 'pre', 'ul',
-		// Inline Text Semantics
-		'a', 'abbr', 'b', 'bdi', 'bdo', 'br', 'cite', 'code', 'data', 'dfn', 'em', 'i', 'kbd', 'mark', 'q','rb',
-		'rp', 'rt', 'rtc', 'ruby', 's', 'samp', 'small', 'span', 'strong', 'sub', 'sup', 'time', 'u', 'var', 'wbr',
-		// Image and Multimedia
-		'area', 'audio', 'img', 'map', 'track', 'video',
-		// Embedded Content
-		'embed', 'iframe', 'object', 'param', 'picture', 'source',
-		// Scripting
-		'canvas', 'noscript', 'script',
-		// Demarcating Edits
-		'del', 'ins',
-		// Table Content
-		'caption', 'col', 'colgroup', 'table', 'tbody', 'td', 'tfoot', 'th', 'thead', 'tr',
-		// Forms
-		'button', 'datalist', 'fieldset', 'form', 'input', 'label', 'legend', 'meter', 'optgroup', 'option', 'output', 'progress', 'select', 'textarea',
-		// Interactive Elements
-		'details', 'dialog', 'menu', 'summary',
-		// Web Components
-		'slot', 'template',
-		// SVG
-		...svgTags
-	];
+exports.attrList = [
+	...baseAttr,
+	// SVG
+	...svgAttr,
+	// Aria
+	...ariaAttr,
+	// Global Event Handlers
+	...eventAttr
+];
 
-	const attrList = [
-		// A
-		'abbr', 'accept', 'acceptCharset', 'accessKey', 'action', 'allowFullScreen', 'allowTransparency', 'alt', 'as', 'async', 'autoComplete', 'autoFocus', 'autoPlay',
-		// C  NOTE: 'class' is replaced with 'classes' due to name collision.
-		'cellPadding', 'cellSpacing', 'challenge', 'charset', 'checked', 'cite', 'className', 'cols', 'colSpan', 'command', 'content', 'contentEditable',
-		'contextMenu', 'controls', 'coords', 'crossOrigin',
-		// D
-		'data', 'dateTime', 'default', 'defer', 'dir', 'disabled', 'download', 'draggable', 'dropzone',
-		// E
-		'encType',
-		// F  NOTE: 'for' is replaced with 'isFor' due to name collision.
-		'form', 'formAction', 'formEncType', 'formMethod', 'formNoValidate', 'formTarget', 'frameBorder',
-		// H
-		'headers', 'height', 'hidden', 'high', 'href', 'hrefLang', 'htmlFor', 'httpEquiv',
-		// I
-		'icon', 'id', 'inputMode', 'isMap', 'itemId', 'itemProp', 'itemRef', 'itemScope', 'itemType',
-		// K
-		'kind',
-		// L
-		'label', 'lang', 'list', 'loop',
-		// M
-		'manifest', 'max', 'maxLength', 'media', 'mediaGroup', 'method', 'min', 'minLength', 'multiple', 'muted',
-		// N
-		'name', 'noValidate',
-		// O
-		'open', 'optimum',
-		// P
-		'pattern', 'ping', 'placeholder', 'poster', 'preload',
-		// R
-		'radioGroup', 'readOnly', 'rel', 'required', 'role', 'rows', 'rowSpan',
-		// S
-		'sandbox', 'scope', 'scoped', 'scrolling', 'seamless', 'selected', 'shape', 'size', 'sizes', 'sortable', 'span', 'spellCheck', 'src', 'srcDoc', 'srcSet',
-		'start', 'step', 'style',
-		// T
-		'tabIndex', 'target', 'title', 'translate', 'type', 'typeMustMatch',
-		// U
-		'useMap',
-		// V
-		'value', 'version',
-		// W
-		'width', 'wmode', 'wrap',
-		// SVG
-		...svgAttr,
-		// Aria
-		...ariaAttr
-	];
-
-	return {
-		tags: tagList,
-		attr: attrList
-	}
-}
-
-},{"./aria":1,"./svg":3}],3:[function(require,module,exports){
+},{"./aria":1,"./base":2,"./events":4,"./svg":5}],4:[function(require,module,exports){
+exports.eventAttr = [
+	'onabort',
+	'onanimationcancel ',
+	'onanimationend ',
+	'onanimationiteration ',
+	'onanimationstart ',
+	'onauxclick ',
+	'onblur',
+	'onerror',
+	'onfocus',
+	'oncancel',
+	'oncanplay',
+	'oncanplaythrough',
+	'onchange',
+	'onclick',
+	'onclose',
+	'oncontextmenu',
+	'oncuechange',
+	'ondblclick',
+	'ondrag',
+	'ondragend',
+	'ondragenter',
+	'ondragexit',
+	'ondragleave',
+	'ondragover',
+	'ondragstart',
+	'ondrop',
+	'ondurationchange',
+	'onemptied',
+	'onended',
+	'onformdata',
+	'ongotpointercapture',
+	'oninput',
+	'oninvalid',
+	'onkeydown',
+	'onkeypress',
+	'onkeyup',
+	'onload',
+	'onloadeddata',
+	'onloadedmetadata',
+	'onloadend',
+	'onloadstart',
+	'onlostpointercapture',
+	'onmousedown',
+	'onmouseenter',
+	'onmouseleave',
+	'onmousemove',
+	'onmouseout',
+	'onmouseover',
+	'onmouseup',
+	'onmousewheel  ',
+	'onwheel',
+	'onpause',
+	'onplay',
+	'onplaying',
+	'onpointerdown',
+	'onpointermove',
+	'onpointerup',
+	'onpointercancel',
+	'onpointerover',
+	'onpointerout',
+	'onpointerenter',
+	'onpointerleave',
+	'onpointerlockchange ',
+	'onpointerlockerror ',
+	'onprogress',
+	'onratechange',
+	'onreset',
+	'onresize',
+	'onscroll',
+	'onseeked',
+	'onseeking',
+	'onselect',
+	'onselectstart',
+	'onselectionchange',
+	'onshow',
+	'onsort ',
+	'onstalled',
+	'onsubmit',
+	'onsuspend',
+	'ontimeupdate',
+	'onvolumechange',
+	'ontouchcancel  ',
+	'ontouchend  ',
+	'ontouchmove  ',
+	'ontouchstart  ',
+	'ontransitioncancel',
+	'ontransitionend',
+	'ontransitionrun',
+	'ontransitionstart',
+	'onwaiting'
+];
+},{}],5:[function(require,module,exports){
 exports.svgTags = [
 	// A
 	'a', 'animate', 'animateMotion', 'animateTransform',
@@ -247,7 +343,7 @@ exports.svgAttr = [
 	// Z
 	'z', 'zoomAndPan'
 ];
-},{}],4:[function(require,module,exports){
+},{}],6:[function(require,module,exports){
 function kebabeToCamel(s) {
   return s.replace(/([-_][a-z])/ig, ($1) => {
     return $1.toUpperCase().replace('-', '');
@@ -270,7 +366,7 @@ function createAttributes(arr) {
 }
 
 module.exports = (attrList) => createAttributes(attrList);
-},{}],5:[function(require,module,exports){
+},{}],7:[function(require,module,exports){
 class Element {
 	constructor(name, attr, internal) {
 		this.tag = name;
@@ -303,15 +399,14 @@ function generateTags(arr) {
 }
 
 module.exports = (tagList) => generateTags(tagList);
-},{}],6:[function(require,module,exports){
-const haipa = (useSvg) => {
-	const data = require('./data/data')(useSvg);
-	return {
-		tags: require('./factories/tags')(data.tags),
-		attr: require('./factories/attr')(data.attr)
-	}
+},{}],8:[function(require,module,exports){
+const { tagList, attrList } = require('./data/data');
+
+const haipa = {
+	tags: require('./factories/tags')(tagList),
+	attr: require('./factories/attr')(attrList)
 }
 
 module.exports = haipa;
-},{"./data/data":2,"./factories/attr":4,"./factories/tags":5}]},{},[6])(6)
+},{"./data/data":3,"./factories/attr":6,"./factories/tags":7}]},{},[8])(8)
 });
